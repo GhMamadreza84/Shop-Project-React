265efa858d25d7b1c4122b1122f6ac554abddfd7
Gh Mamadreza <ghmamadreza84@gmail.com>
2024-05-15 22:04:49 +0200

    add class name to products page element

--- src/pages/ProductsPage.jsx
+++ src/pages/ProductsPage.jsx
@@ -3,8 +3,8 @@ const ProductsPage = () => {
   const products = useProducts();
   console.log(products);
   return (
-    <div>
-      <div>
+    <div className={styles.container}>
+      <div className={styles.products}>
         {products.map((p) => (
           <p key={p.id}>{p.title}</p>
         ))}
