691bf4a678ab364e975bf8c88f04ad85f8d81115
Gh Mamadreza <ghmamadreza84@gmail.com>
2024-05-15 21:58:42 +0200

    create function for pass context to all routes and component with custome hook useProducts

--- src/context/ProductContext.jsx
+++ src/context/ProductContext.jsx
@@ -1,4 +1,4 @@
-import { createContext, useEffect, useState } from "react";
+import { createContext, useContext, useEffect, useState } from "react";
 import api from "../services/config";
 
 const ProductContext = createContext();
@@ -15,7 +15,17 @@ const ProductProvider = ({ children }) => {
     };
     fetchProducts();
   }, []);
-  return <ProductContext.Provider value={products}>{children}</ProductContext.Provider>;
+  return (
+    <ProductContext.Provider value={products}>
+      {children}
+    </ProductContext.Provider>
+  );
+};
+
+const useProducts = () => {
+  const products = useContext(ProductContext);
+  return products;
 };
 
 export default ProductProvider;
+export { useProducts };
