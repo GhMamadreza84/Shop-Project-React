764cfe185d9be3d41a2fb299bd05c4b637dfeade
Gh Mamadreza <ghmamadreza84@gmail.com>
2024-05-15 18:09:07 +0200

    navigate routes to produst page

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,15 @@
-import { Route, Routes } from "react-router-dom"
-import ProductsPage from "./pages/productsPage"
-
+import { Route, Routes } from "react-router-dom";
+import ProductsPage from "./pages/productsPage";
 
 function App() {
   return (
     <>
       <Routes>
+        <Route path="/" element={<Navigate to={<ProductsPage />} />} />
         <Route path="/products" element={<ProductsPage />} />
       </Routes>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
