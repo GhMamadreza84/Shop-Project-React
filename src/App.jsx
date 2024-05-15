7853c67c02c864a7b480674ebf7b018792792d7a
Gh Mamadreza <ghmamadreza84@gmail.com>
2024-05-15 18:13:32 +0200

    create route for checkout page

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { Route, Routes, Navigate } from "react-router-dom";
 import ProductsPage from "./pages/ProductsPage";
 import DetailsPage from "./pages/DetailsPage";
+import CheckoutPage from "./pages/CheckoutPage";
 
 function App() {
   return (
@@ -9,6 +10,8 @@ function App() {
         <Route path="/" element={<Navigate to="/products" replace />} />
         <Route path="/products" element={<ProductsPage />} />
         <Route path="/products/:id" element={<DetailsPage />} />
+        <Route path="/products/:id" element={<DetailsPage />} />
+        <Route path="/checkout" element={<CheckoutPage />} />
       </Routes>
     </>
   );
