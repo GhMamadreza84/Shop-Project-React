5ea5a427b7c270a1353e9e5e7dd07313b032166a
Gh Mamadreza <ghmamadreza84@gmail.com>
2024-05-15 21:56:27 +0200

    wrap productsProvider all routes in app

--- src/App.jsx
+++ src/App.jsx
@@ -3,18 +3,21 @@ import ProductsPage from "./pages/ProductsPage";
 import DetailsPage from "./pages/DetailsPage";
 import CheckoutPage from "./pages/CheckoutPage";
 import PageNotFound from "./pages/404";
+import ProductProvider from "./context/ProductContext";
 
 function App() {
   return (
     <>
-      <Routes>
-        <Route path="/" element={<Navigate to="/products" replace />} />
-        <Route path="/products" element={<ProductsPage />} />
-        <Route path="/products/:id" element={<DetailsPage />} />
-        <Route path="/products/:id" element={<DetailsPage />} />
-        <Route path="/checkout" element={<CheckoutPage />} />
-        <Route path="/*" element={<PageNotFound />} />
-      </Routes>
+      <ProductProvider>
+        <Routes>
+          <Route path="/" element={<Navigate to="/products" replace />} />
+          <Route path="/products" element={<ProductsPage />} />
+          <Route path="/products/:id" element={<DetailsPage />} />
+          <Route path="/products/:id" element={<DetailsPage />} />
+          <Route path="/checkout" element={<CheckoutPage />} />
+          <Route path="/*" element={<PageNotFound />} />
+        </Routes>
+      </ProductProvider>
     </>
   );
 }
