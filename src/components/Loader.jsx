02ffdca6bceb800846c2b88573002d8f3c03a24c
Gh Mamadreza <ghmamadreza84@gmail.com>
2024-05-16 10:46:04 +0200

    import styles and add class name to loader component

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -1,10 +1,16 @@
 import { RotatingLines } from "react-loader-spinner";
+import styles from "./Loader.module.css";
 const Loader = () => {
   return (
-    <div>
-      <RotatingLines width="100px" height="100px" strokeWidth="3" strokeColor="#fe5d42" />
+    <div className={styles.loader}>
+      <RotatingLines
+        width="100px"
+        height="100px"
+        strokeWidth="3"
+        strokeColor="#fe5d42"
+      />
     </div>
   );
-}
+};
 
 export default Loader;
