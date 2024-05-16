017ebc901400a2f09fcb656cc3e602cc727fdff2
Gh Mamadreza <ghmamadreza84@gmail.com>
2024-05-16 10:06:08 +0200

    pass data with props to Card

--- src/pages/ProductsPage.jsx
+++ src/pages/ProductsPage.jsx
@@ -1,3 +1,4 @@
+import Card from "../components/Card";
 import { useProducts } from "../context/ProductContext";
 import styles from "./ProductsPage.module.css";
 const ProductsPage = () => {
@@ -8,7 +9,7 @@ const ProductsPage = () => {
       <div className={styles.products}>
         {!products.length && <p>Loading ...</p>}
         {products.map((p) => (
-          <p key={p.id}>{p.title}</p>
+          <Card key={p.id} data={p} />
         ))}
       </div>
       <div>sideBar</div>
