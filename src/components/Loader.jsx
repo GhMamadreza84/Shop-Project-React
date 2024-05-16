27f149b717edf5eb47d572772ccbaac9f7d16f3e
Gh Mamadreza <ghmamadreza84@gmail.com>
2024-05-16 10:45:20 +0200

    add rotatingLines to loader component

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -1,7 +1,8 @@
-
+import { RotatingLines } from "react-loader-spinner";
 const Loader = () => {
   return (
     <div>
+      <RotatingLines width="100px" height="100px" strokeWidth="3" strokeColor="#fe5d42" />
     </div>
   );
 }
