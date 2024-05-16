efa0f84cbba4e490560fe6c5d42bc024c7c23a74
Gh Mamadreza <ghmamadreza84@gmail.com>
2024-05-16 10:50:26 +0200

    create div input and button

--- src/pages/ProductsPage.jsx
+++ src/pages/ProductsPage.jsx
@@ -1,20 +1,30 @@
+import { useProducts } from "../context/ProductContext";
+import { ImSearch } from "react-icons/im";
+
 import Card from "../components/Card";
 import Loader from "../components/Loader";
-import { useProducts } from "../context/ProductContext";
 import styles from "./ProductsPage.module.css";
 const ProductsPage = () => {
   const products = useProducts();
   console.log(products);
   return (
-    <div className={styles.container}>
-      <div className={styles.products}>
-        {!products.length && <Loader />}
-        {products.map((p) => (
-          <Card key={p.id} data={p} />
-        ))}
+    <>
+      <div>
+        <input type="text" placeholder="Search..." />
+        <button>
+          <ImSearch />
+        </button>
       </div>
-      <div>sideBar</div>
-    </div>
+      <div className={styles.container}>
+        <div className={styles.products}>
+          {!products.length && <Loader />}
+          {products.map((p) => (
+            <Card key={p.id} data={p} />
+          ))}
+        </div>
+        <div>sideBar</div>
+      </div>
+    </>
   );
 };
 
