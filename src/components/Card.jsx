aff74a4348fc1d2fec34078b4a74b32b3bb3ebff
Gh Mamadreza <ghmamadreza84@gmail.com>
2024-05-16 10:18:29 +0200

    edit

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,17 +1,9 @@
 import { Link } from "react-router-dom";
 import { TbListDetails } from "react-icons/tb";
 const Card = ({ data }) => {
-  const { id, title, image, price } = data;
   return (
     <div>
-      <img src={image} alt={title} />
-      <h3>{title}</h3>
-      <p>{price} $</p>
-      <div>
-        <Link to={`/products/${id}`}>
-          <TbListDetails />
-        </Link>
-      </div>
+     
     </div>
   );
 };
