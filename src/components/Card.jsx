34b1f06678898e4822eb645ced2641db3f631b3e
Gh Mamadreza <ghmamadreza84@gmail.com>
2024-05-16 10:37:57 +0200

    edit card component

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -3,21 +3,23 @@ import { TbListDetails } from "react-icons/tb";
 import { TbShoppingBagCheck } from "react-icons/tb";
 import { shortenText } from "../helper/Helper";
 
-import styles from "./Card.module.css"
+import styles from "./Card.module.css";
 const Card = ({ data }) => {
   const { id, title, image, price } = data;
   return (
     <div className={styles.card}>
-      <img src={image} alt={title}  />
+      <img src={image} alt={title} />
       <h3>{shortenText(title)}</h3>
       <p>{price} $</p>
       <div className={styles.actions}>
         <Link to={`/products/${id}`}>
           <TbListDetails />
         </Link>
-        <button>
-          <TbShoppingBagCheck />
-        </button>
+        <div>
+          <button>
+            <TbShoppingBagCheck />
+          </button>
+        </div>
       </div>
     </div>
   );
