6eacf987b21b644ebe1194b2dc942d852080e9e1
Gh Mamadreza <ghmamadreza84@gmail.com>
2024-05-16 10:28:06 +0200

    move helper function to helper folder

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,7 +1,7 @@
 import { Link } from "react-router-dom";
 import { TbListDetails } from "react-icons/tb";
 import { TbShoppingBagCheck } from "react-icons/tb";
-import { shortenText } from "./Helper";
+import { shortenText } from "../helper/Helper";
 const Card = ({ data }) => {
   const { id, title, image, price } = data;
   return (
