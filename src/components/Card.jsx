8e0f21e8c82175b8d1a3c1afeb8480da25b491e7
Gh Mamadreza <ghmamadreza84@gmail.com>
2024-05-16 10:19:45 +0200

    restore to defualt card component

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,9 +1,17 @@
 import { Link } from "react-router-dom";
 import { TbListDetails } from "react-icons/tb";
 const Card = ({ data }) => {
+  const { id, title, image, price } = data;
   return (
     <div>
-     
+      <img src={image} alt={title} />
+      <h3>{title}</h3>
+      <p>{price} $</p>
+      <div>
+        <Link to={`/products/${id}`}>
+          <TbListDetails />
+        </Link>
+      </div>
     </div>
   );
 };
