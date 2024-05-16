d70eacb112a9d9253bd88e624231dad8e84e342e
Gh Mamadreza <ghmamadreza84@gmail.com>
2024-05-16 10:21:27 +0200

    import icons

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,10 +1,11 @@
 import { Link } from "react-router-dom";
 import { TbListDetails } from "react-icons/tb";
+import { TbShoppingBagCheck } from "react-icons/tb";
 const Card = ({ data }) => {
   const { id, title, image, price } = data;
   return (
     <div>
-      <img src={image} alt={title} />
+      <img src={image} alt={title} style={{width:"150px"}} />
       <h3>{title}</h3>
       <p>{price} $</p>
       <div>
