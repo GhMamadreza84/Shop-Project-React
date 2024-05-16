ee44512a89c313f0393ade71ddd444c2672f2e33
Gh Mamadreza <ghmamadreza84@gmail.com>
2024-05-16 10:28:49 +0200

    create card.css and import this in Card

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -2,6 +2,8 @@ import { Link } from "react-router-dom";
 import { TbListDetails } from "react-icons/tb";
 import { TbShoppingBagCheck } from "react-icons/tb";
 import { shortenText } from "../helper/Helper";
+
+import styles from "./Card.module.css"
 const Card = ({ data }) => {
   const { id, title, image, price } = data;
   return (
