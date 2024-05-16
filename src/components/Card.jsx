1639437beed78b327914e3e93445828bf73d3af4
Gh Mamadreza <ghmamadreza84@gmail.com>
2024-05-16 10:11:39 +0200

    add price image and title in card

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,6 +1,19 @@
+import { Link } from "react-router-dom";
+import { TbListDetails } from "react-icons/tb";
 const Card = ({ data }) => {
-  console.log(data);
-  return <div>card</div>;
+  const { id, title, image, price } = data;
+  return (
+    <div>
+      <img src={image} alt={title} />
+      <h3>{title}</h3>
+      <p>{price}</p>
+      <div>
+        <Link to={`/products/${id}`}>
+          <TbListDetails />
+        </Link>
+      </div>
+    </div>
+  );
 };
 
 export default Card;
