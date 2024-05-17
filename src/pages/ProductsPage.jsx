1c8bb9adb41c908d505ceb65b761db4564ace86c
Gh Mamadreza <ghmamadreza84@gmail.com>
2024-05-17 17:44:44 +0200

    edit sideBar List

--- src/pages/ProductsPage.jsx
+++ src/pages/ProductsPage.jsx
@@ -61,7 +61,7 @@ const ProductsPage = () => {
           </div>
           <ul onClick={categoryHandler}>
             <li>All</li>
-            <li>Electeronics</li>
+            <li>Electronics</li>
             <li>Jewelery</li>
             <li>Men's Clothing</li>
             <li>Women's Clothing</li>
