26cd3781ea7414adba20d682b9a14609e66196a7
Gh Mamadreza <ghmamadreza84@gmail.com>
2024-05-17 17:29:27 +0200

    complate opration filtering search

--- src/helper/Helper.js
+++ src/helper/Helper.js
@@ -3,6 +3,9 @@ const shortenText = (text) => {
 };
 const searchProducts = (products, search) => {
   if (!search) return products;
-  const searchedProducts = products;
+  const searchedProducts = products.filter((p) =>
+    p.title.toLowerCase().includes(search)
+  );
+  return searchedProducts;
 };
-export { shortenText };
+export { shortenText , searchProducts };
