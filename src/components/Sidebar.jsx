2b677f427e0abcd9907c3666fc74b490a0df1227
Gh Mamadreza <ghmamadreza84@gmail.com>
2024-05-18 22:56:04 +0200

    create constant folder and create list.js file and move categories list to list files

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -3,13 +3,7 @@ import { createQueryObject } from "../helper/Helper";
 
 import styles from "./Sidebar.module.css";
 
-const categories = [
-  { id: 1, type: "All" },
-  { id: 2, type: "Electronics" },
-  { id: 3, type: "Jewelery" },
-  { id: 4, type: "Men's Clothing" },
-  { id: 5, type: "Women's Clothing" },
-];
+import { categories } from "../constant/list";
 const Sidebar = ({ query, setQuery }) => {
   const categoryHandler = (event) => {
     const { tagName } = event.target;
