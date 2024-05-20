89f1fda96bc6bd40f30b767c6c1914be309c4709
ghmamadreza2 <ghmamadreza84@gmail.com>
2024-05-19 23:40:09 -0700

    create reducer in cartcontext

--- src/context/CartContext.jsx
+++ src/context/CartContext.jsx
@@ -1,9 +1,10 @@
+import { useReducer } from "react";
+import { createContext } from "react";
 
-const CartProvider = () => {
-  return (
-    <div>
-    </div>
-  );
-}
+const CartContext = createContext;
+const CartProvider = ({ children }) => {
+  const [state, dispatch] = useReducer(reducer, initialState);
+  return <CartContext.Provider>{children}</CartContext.Provider>;
+};
 
 export default CartProvider;
