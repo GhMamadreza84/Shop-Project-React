0b2be4f7b52cf4242a729ef500304fa3fbc37f1b
ghmamadreza2 <ghmamadreza84@gmail.com>
2024-05-21 12:59:21 -0700

    add loading when fetching data in details page

--- src/pages/DetailsPage.jsx
+++ src/pages/DetailsPage.jsx
@@ -1,14 +1,12 @@
 import { useParams } from "react-router-dom";
 import { useProductDetils } from "../context/ProductContext";
+import Loader from "../components/Loader";
 
 const DetailsPage = () => {
-  const {id} = useParams()
-  const productDetails = useProductDetils(+id)
-  return (
-    <div>
-      DetailsPage - {id}
-    </div>
-  );
-}
+  const { id } = useParams();
+  const productDetails = useProductDetils(+id);
+  if (!productDetails) return <Loader />;
+  return <div>DetailsPage - {id}</div>;
+};
 
 export default DetailsPage;
