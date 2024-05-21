cf9e39d95c1c1b5e2228bb32ddefcee1a68f1893
ghmamadreza2 <ghmamadreza84@gmail.com>
2024-05-21 03:26:21 -0700

    add and import icon in layout

--- src/layout/Layout.jsx
+++ src/layout/Layout.jsx
@@ -1,10 +1,13 @@
 import { Link } from "react-router-dom";
-
+import { PiShoppingCartSimpleBold } from "react-icons/pi";
 const Layout = ({ children }) => {
   return (
     <>
       <header>
         <Link to="/products">Gh Shop</Link>
+        <Link to="/checkout">
+          <PiShoppingCartSimpleBold />
+        </Link>
       </header>
       {children}
       <footer></footer>
