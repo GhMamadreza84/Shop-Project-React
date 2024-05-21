904747d548a4be28864f9cefadff5b7834aab2a5
ghmamadreza2 <ghmamadreza84@gmail.com>
2024-05-21 03:28:02 -0700

    wrap layout in app

--- src/App.jsx
+++ src/App.jsx
@@ -5,20 +5,23 @@ import CheckoutPage from "./pages/CheckoutPage";
 import PageNotFound from "./pages/404";
 import ProductProvider from "./context/ProductContext";
 import CartProvider from "./context/CartContext";
+import Layout from "./layout/Layout";
 
 function App() {
   return (
     <>
       <CartProvider>
         <ProductProvider>
-          <Routes>
-            <Route path="/" element={<Navigate to="/products" replace />} />
-            <Route path="/products" element={<ProductsPage />} />
-            <Route path="/products/:id" element={<DetailsPage />} />
-            <Route path="/products/:id" element={<DetailsPage />} />
-            <Route path="/checkout" element={<CheckoutPage />} />
-            <Route path="/*" element={<PageNotFound />} />
-          </Routes>
+          <Layout>
+            <Routes>
+              <Route path="/" element={<Navigate to="/products" replace />} />
+              <Route path="/products" element={<ProductsPage />} />
+              <Route path="/products/:id" element={<DetailsPage />} />
+              <Route path="/products/:id" element={<DetailsPage />} />
+              <Route path="/checkout" element={<CheckoutPage />} />
+              <Route path="/*" element={<PageNotFound />} />
+            </Routes>
+          </Layout>
         </ProductProvider>
       </CartProvider>
     </>
