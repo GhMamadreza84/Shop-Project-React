8ec7f74a78e9e960e7058d33831f283776d6abb7
ghmamadreza2 <ghmamadreza84@gmail.com>
2024-05-22 14:30:07 -0700

    import basket  side bar in check out page

--- src/pages/CheckoutPage.jsx
+++ src/pages/CheckoutPage.jsx
@@ -2,11 +2,13 @@ import { useCart } from "../context/CartContext";
 import BasketCard from "../components/BasketCard";
 import styles from "./Checkout.module.css";
 import empty from "../assets/empty.jpg"
+import SideBasketBar from "../components/SideBasketBar";
 const CheckoutPage = () => {
   const [state, dispatch] = useCart();
   const clickHandler = (type, payload) => dispatch({ type, payload });
   return (
     <div>
+      <SideBasketBar />
       {state.selectedItems.length ? (
         <div>
           {state.selectedItems.map((product) => (
