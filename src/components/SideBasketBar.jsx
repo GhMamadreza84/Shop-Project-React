4ad05d39738f23f7425ef34879396790e93502fe
ghmamadreza2 <ghmamadreza84@gmail.com>
2024-05-22 14:33:51 -0700

    import icon in side bar basketcard

--- src/components/SideBasketBar.jsx
+++ src/components/SideBasketBar.jsx
@@ -1,8 +1,14 @@
-
+import { TbChecklist } from "react-icons/tb";
 const SideBasketBar = ({state}) => {
   return (
     <div>
-      
+      <div>
+        <TbChecklist />
+        <p>Total :</p>
+        <span>{state.total}</span>
+      </div>
+      <div></div>
+      <div></div>
     </div>
   );
 }
