4844fb5f619b3b96c29ac409f7f86dc7c0fc5a09
ghmamadreza2 <ghmamadreza84@gmail.com>
2024-05-22 14:43:57 -0700

    edit if for when basket card is empty

--- src/pages/CheckoutPage.jsx
+++ src/pages/CheckoutPage.jsx
@@ -1,30 +1,32 @@
 import { useCart } from "../context/CartContext";
 import BasketCard from "../components/BasketCard";
 import styles from "./Checkout.module.css";
-import empty from "../assets/empty.jpg"
+import empty from "../assets/empty.jpg";
 import SideBasketBar from "../components/SideBasketBar";
 const CheckoutPage = () => {
   const [state, dispatch] = useCart();
   const clickHandler = (type, payload) => dispatch({ type, payload });
+  if (!state.itemsCounter) {
+    return (
+      <div className={styles.empty}>
+        <h1>Basket Card is Empty 😢</h1>
+        <img src={empty} alt="empty" />
+      </div>
+    );
+  }
   return (
     <div>
       <SideBasketBar state={state} clickHandler={clickHandler} />
-      {state.selectedItems.length ? (
-        <div>
-          {state.selectedItems.map((product) => (
-            <BasketCard
-              key={product.id}
-              data={product}
-              clickHandler={clickHandler}
-            />
-          ))}
-        </div>
-      ) : (
-        <div className={styles.empty}>
-          <h1 >Basket Card is Empty 😢</h1>
-          <img src={empty} alt="empty" />
-        </div>
-      )}
+
+      <div>
+        {state.selectedItems.map((product) => (
+          <BasketCard
+            key={product.id}
+            data={product}
+            clickHandler={clickHandler}
+          />
+        ))}
+      </div>
     </div>
   );
 };
