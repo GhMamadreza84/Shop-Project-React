e2c6a89f1bd82cf61a19061ce574938255eb5c30
ghmamadreza2 <ghmamadreza84@gmail.com>
2024-05-22 10:08:13 -0700

    edit basket card

--- src/components/BasketCard.jsx
+++ src/components/BasketCard.jsx
@@ -1,7 +1,11 @@
+import { shortenText } from "../helper/Helper";
 
 const BasketCard = ({data}) => {
+  const {image,title}=data;
   return (
     <div>
+      <img src={image} alt={title} />
+      <p>{shortenText(title)}</p>
     </div>
   );
 }
