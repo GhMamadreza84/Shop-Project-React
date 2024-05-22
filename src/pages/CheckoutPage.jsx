0c13dffbfca3be3255bb24b6f8161cdf15144123
ghmamadreza2 <ghmamadreza84@gmail.com>
2024-05-22 10:19:43 -0700

    create click handler button in check out page and pass with props to basket card

--- src/pages/CheckoutPage.jsx
+++ src/pages/CheckoutPage.jsx
@@ -3,10 +3,13 @@ import { useCart } from "../context/CartContext";
 
 const CheckoutPage = () => {
   const [state, dispatch] = useCart();
+  const clickHandler = (type, payload) => dispatch({ type, payload });
   return (
     <div>
       <div>
-        {state.selectedItems.map(product=><BasketCard key={product.id} data={product} />)}
+        {state.selectedItems.map((product) => (
+          <BasketCard key={product.id} data={product} />
+        ))}
       </div>
     </div>
   );
