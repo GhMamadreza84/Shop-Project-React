482cf6b077088cc19c8f7b6e4da810cd235ef4d8
ghmamadreza2 <ghmamadreza84@gmail.com>
2024-05-22 10:20:51 -0700

    edit on click button in basket card

--- src/components/BasketCard.jsx
+++ src/components/BasketCard.jsx
@@ -1,7 +1,7 @@
 import { shortenText } from "../helper/Helper";
 import { MdDeleteOutline } from "react-icons/md";
 const BasketCard = ({ data }) => {
-  const { image, title, price, quantity } = data;
+  const { image, title, price, quantity,clickHandler } = data;
   return (
     <div>
       <img src={image} alt={title} />
@@ -9,13 +9,13 @@ const BasketCard = ({ data }) => {
       <p>{price}</p>
       <div>
         {quantity === 1 && (
-          <button onClick={()=>clickHandler("REMOVE_ITEM")}>
+          <button onClick={()=>clickHandler("REMOVE_ITEM",data)}>
             <MdDeleteOutline />
           </button>
         )}
-        {quantity > 1 && <button onClick={()=>clickHandler("DECREASE")}>-</button>}
+        {quantity > 1 && <button onClick={()=>clickHandler("DECREASE",data)}>-</button>}
         <span>{quantity}</span>
-        <button onClick={()=>clickHandler("INCREASE")}>+</button>
+        <button onClick={()=>clickHandler("INCREASE",data)}>+</button>
       </div>
     </div>
   );
